8f906bbf5c2e17bb99be6099e066cc3a57991596
adarshsingh1407 <adarshsingh1407@gmail.com>
2018-12-06 23:27:47 +0530

    fixed e2e test errors

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('neo-angular7-noob App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display app title correctly', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('NEO Angular7 Noob');
   });
 });
